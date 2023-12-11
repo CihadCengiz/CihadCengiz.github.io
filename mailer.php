4d83a4207c8e8bc7c999c3fade1172529065804e
CihadCengiz <ccengizstu@gmail.com>
2023-12-11 17:45:47 +0300

    fixed mailer

--- mailer.php
+++ mailer.php
@@ -6,9 +6,14 @@ if($_POST) {
   $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
   $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
   $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+  $headers = array(
+    'From' => 'webmaster@example.com',
+    'Reply-To' => 'webmaster@example.com',
+    'X-Mailer' => 'PHP/' . phpversion()
+);
   $body = "Name: $name\nE-mail: $email\nSubject: $subject\nMessage: $message";
   
-  if(@mail($to, $subject, $body)) {
+  if(@mail($to, $subject, $body, $headers)) {
     $output = json_encode(array('success' => true));
     die($output);
   } else {
